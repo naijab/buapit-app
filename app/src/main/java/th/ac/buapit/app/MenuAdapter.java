4283bab09123b1adc2871abdf23cff4b05a9fef3
Nattapon Pondongnok <nattapon.jab@gmail.com>
2016-10-05 04:13:01 +0700

    New notification

---
+++ app/src/main/java/th/ac/buapit/app/MenuAdapter.java
@@ -0,0 +1,47 @@
+package th.ac.buapit.app;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+
+public class MenuAdapter extends BaseAdapter {
+
+    private final Context mContext;
+    private final Book[] books;
+
+    // 1
+    public MenuAdapter(Context context, Book[] books) {
+        this.mContext = context;
+        this.books = books;
+    }
+
+    // 2
+    @Override
+    public int getCount() {
+        return books.length;
+    }
+
+    // 3
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // 4
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    // 5
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        TextView dummyTextView = new TextView(mContext);
+        dummyTextView.setText(String.valueOf(position));
+        return dummyTextView;
+    }
+
+}
